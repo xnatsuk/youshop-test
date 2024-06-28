c18311f0dcaa77b0694f94167b574c72551f485d
xnatsuk <xnatsuk@gmail.com>
2024-06-28 00:02:19 -0300

    feat: add error page

---
+++ src/pages/[...all].vue
@@ -0,0 +1,9 @@
+<template>
+  <v-empty-state
+    headline="Opa, Erro 404"
+    title="Essa página não existe"
+    text="Tente acessar outra página ou volte para a página inicial."
+    icon="mdi-emoticon-confused"
+    color="indigo-lighten-1"
+  />
+</template>
