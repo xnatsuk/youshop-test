590a31d4d62b6129dddb26d02fe6448da24eed93
xnatsuk <xnatsuk@gmail.com>
2024-06-28 00:41:30 -0300

    feat: add products page

---
+++ src/components/products/ProductCard.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { ProductCard } from '@/types'
+
+const products = defineModel<ProductCard[]>('products')
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="item in products"
+        :key="item.name"
+        cols="6"
+      >
+        <v-card>
+          <v-img
+            :src="item.image"
+            class="align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+            cover
+          >
+            <v-card-title class="text-white text-align-start">
+              {{ item.name }}
+            </v-card-title>
+          </v-img>
+
+          <v-card-actions>
+            <span class="text-h6 text-indigo">
+              R$ {{ item.price }}
+            </span>
+
+            <v-spacer />
+
+            <router-link :to="`/checkout/${item.offerCode}`">
+              <v-btn color="indigo" variant="tonal" prepend-icon="mdi-cart-outline">
+                Comprar
+              </v-btn>
+            </router-link>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+
+const { productList } = storeToRefs(useProductStore())
+</script>
+
+<template>
+  <Suspense>
+    <ProductCard :products="productList" />
+  </Suspense>
+</template>
