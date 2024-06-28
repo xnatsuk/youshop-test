988bfdf3eb33da928684f68ab8fb864299798d37
xnatsuk <xnatsuk@gmail.com>
2024-06-28 00:39:56 -0300

    feat: fetch data before each route

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,3 +7,14 @@ const router = createRouter({
 })
 
 export default router
+
+router.beforeEach(async (to, from, next) => {
+  const store = useProductStore()
+  if (to.name === '/checkout/[code]')
+    await store.setProduct(to.params.code)
+
+  if (to.name === '/')
+    await store.setProductList()
+
+  next()
+})
