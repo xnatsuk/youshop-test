a0ed239475702364482e99321e3b2fba665fa895
xnatsuk <xnatsuk@gmail.com>
2024-06-29 04:17:16 -0300

    fix: update url

--- src/composables/products.ts
+++ src/composables/products.ts
@@ -3,7 +3,9 @@ import type { Product } from '@/types'
 const API_URL = import.meta.env.VITE_API_URL
 
 export async function useGetAll() {
-  const { error, data } = await useFetch<Product[]>(`${API_URL}/products`, { method: 'GET' })
+  const { error, data } = await useFetch<Product[]>(`${API_URL}/offers`, {
+    method: 'GET',
+  })
 
   if (error.value !== null)
     console.error(error.value)
@@ -12,7 +14,9 @@ export async function useGetAll() {
 }
 
 export async function useGetByOffer(code: string | number) {
-  const { error, data } = await useFetch<Product>(`${API_URL}/products/${code}`, { method: 'GET' })
+  const { error, data } = await useFetch<Product>(`${API_URL}/offers/${code}`, {
+    method: 'GET',
+  })
 
   if (error.value !== null)
     console.error(error.value)
