1a6660e6f487286076691e65f964dc0b2a5e89f2
xnatsuk <xnatsuk@gmail.com>
2024-06-29 03:46:34 -0300

    feat: add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,51 @@ export interface Product {
 export type ProductCard = Pick<Product, 'offerCode' | 'name' | 'image' | 'price'>
 export interface ProductRequestParams { offerCode: string }
 export type ProductResponse = Product[] | Product
+
+export interface UserInfo {
+  name: string
+  email: string
+  phone: string
+}
+
+export interface ShippingInfo {
+  address: string
+  city: string
+  state: string
+  neighborhood: string
+  postalCode: string
+}
+
+export interface PaymentInfo {
+  cpf: string
+  birthday: string
+  total: number
+  type: 'card' | 'pix' | 'invoice'
+}
+
+export interface CheckoutInfo {
+  client?: UserInfo
+  shipping?: ShippingInfo
+  payment?: PaymentInfo
+  total?: number
+}
+
+export interface CheckoutRequestBody {
+  data: CheckoutInfo
+}
+
+export interface OrderSuccessResponse {
+  orderCode: number
+  total: number
+  shipping: ShippingInfo
+  client: UserInfo
+  invoice?: string
+  pix?: string
+}
+
+export interface OrderErrorResponse {
+  status: string
+  message: string
+}
+
+export type Order = OrderSuccessResponse | OrderErrorResponse
