182327dd3912e67c2ed659fc2b0cc07ca049e729
xnatsuk <xnatsuk@gmail.com>
2024-06-29 03:51:18 -0300

    feat: create form aggregator for values and validation

---
+++ src/composables/formAggregator.ts
@@ -0,0 +1,58 @@
+import type { GenericObject } from 'vee-validate'
+
+type ValidatorMethodType = () => Promise<{ valid: boolean }>
+type ValuesType = GenericObject
+
+export const AddValidatorKey: InjectionKey<
+  (validator: ValidatorMethodType) => void
+> = Symbol('AddValidatorKey')
+
+export function useValidationAggregator() {
+  const validators = ref<ValidatorMethodType[]>([])
+
+  // used by components to add validators to the array
+  const register = (validator: ValidatorMethodType) => {
+    validators.value.push(validator)
+  }
+
+  // provide this aggregator to components
+  provide(AddValidatorKey, register)
+
+  // run all validators
+  const validateAll = async () => {
+    return Promise.all(validators.value.map(v => v()))
+  }
+
+  return {
+    validateAll,
+    register,
+  }
+}
+
+export const AddValuesKey: InjectionKey<
+  (value: ValuesType) => void
+> = Symbol('AddValuesKey')
+
+export function useValuesAggregator() {
+  const values = ref<ValuesType[]>([])
+
+  const register = (value: ValuesType) => {
+    values.value.push(value)
+  }
+
+  provide(AddValuesKey, register)
+
+  const getValues = () => {
+    return values.value.map((v) => {
+      const obj = { ...v }
+      const { ...items } = obj
+
+      return items
+    })
+  }
+
+  return {
+    getValues,
+    register,
+  }
+}
